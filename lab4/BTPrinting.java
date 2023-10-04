4cab6b355ae547080a1b1d6038bcb54bd79481c4
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2023-10-04 14:46:05 -0400

    Update BTPrinting.java

--- lab4/BTPrinting.java
+++ lab4/BTPrinting.java
@@ -1,5 +1,5 @@
-package lab4
 public class BTPrinting extends BinaryTree {
+	protected BinaryTree treeTest;
 	
 	public void preOrder(Node node) {
 		if (node==null)
@@ -9,18 +9,40 @@ public class BTPrinting extends BinaryTree {
 		preOrderPrint(node.right);
 	}
 	
-	private void preOrderPrint() {
+	private void preOrderPrint(Node node) {
 		preOrderPrint(root);
 	}
 	
-	public void printLeft() {
-		if (node==null)
-			return;
-		System.out.println(node.data);
-		leftNodePrint(node.left);
+    private void printLeftRecursive(Node node) {
+        if (node == null) 
+            return;
+        
+        printLeftRecursive(node.left);
+        System.out.print(node.data + " ");
+        printLeftRecursive(node.right);
+    }
+
+    public void printLeft() {
+        printLeftRecursive(root);
+    }
+
+
+	@Override
+	public void testing() {
+	    BinaryTree tree = new BinaryTree();       
+
+	    tree.root = new Node(1);
+	    tree.root.left = new Node(2);
+	    tree.root.right = new Node(3);
+	    tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+	    tree.root.right.right = new Node(7);
+	    
+	    preOrder(tree.root);
+	    printLeftRecursive(tree.root);
+
+		
 	}
-	
-	private void leftNodePrint(Node node) {
-		printLeft(node);
-	}
+
 }
