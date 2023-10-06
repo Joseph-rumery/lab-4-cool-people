57090d02775969fb2f9a065b5b4ea0c84f0c54f9
Joseph-rumery <144263711+Joseph-rumery@users.noreply.github.com>
2023-10-05 20:03:28 -0400

    Update BinaryTree.java

--- lab4/BinaryTree.java
+++ lab4/BinaryTree.java
@@ -1,28 +1,50 @@
-
 public class BinaryTree {
 
 	Node root;
-	
-	public BinaryTree() {
-		root = null;
-	}
-	
- class Node{
-		
-		int data;
-		
-		Node right;
-		Node left;
-		
+
 		public Node() {
 			right = null;
 			left = null;
 		}
-		
-		public Node(int data) {
+
+ class Node{
+
+		int data;
+
+		Node right;
+		Node left;
+
+	 public Node(int data) {
 			this.data = data;
 		}
-		
+
 	}
+
+ public void preOrder() {
+	 
+ }
+ 
+ public void printLeft() {
+	 
+ }
 	
+ public boolean contains(int item) {
+	 return false;
+ }
+ 
+ public int getLevel(int Item) {
+	 return 0;
+ }
+ 
+ public int sumLessThan(int item) {
+	 return 0;
+ }
+ 
+ public int findMax() {
+	 return 0;
+ }
+
+
 }
+
+
